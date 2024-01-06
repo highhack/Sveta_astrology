f69cf2797f815b80e70888c8e6d90c729548d4fa
Gera <gosya85@gmail.com>
2024-01-06 16:21:26 +0200

    x

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,29 @@
 import Heading from "./Heading";
 import { FC } from "react";
 import Image from "next/image";
+import React from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="absolute bottom-0 w-full h-10">
-      <Heading tag={"h3"} text={"created by Gera"} />
-      <Image
-        alt=""
-        src={"/logo.png"}
-        className="w-10 h-5"
-        width={20}
-        height={20}
+    <footer className="absolute flex justify-between bottom-0 w-full h-10 px-4">
+      <div className="flex items-center">
+        <Heading
+          tag={"h2"}
+          className="text-[14px]"
+          text={"©2019-2024 created by "}
+        />
+        <Image
+          alt=""
+          src={"/logo.png"}
+          className="w-8 h-3"
+          width={20}
+          height={20}
+        />
+      </div>
+      <Heading
+        tag={"h2"}
+        className="text-[14px]"
+        text={" All rights reserved. Copyright | Privacy Policy"}
       />
     </footer>
   );
--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,11 +1,18 @@
+import React from "react";
 import { FC } from "react";
+import clsx from "clsx";
 
 type HeadingProps = {
   tag?: string;
   text: string;
+  className?: string;
 };
-const Heading: FC<HeadingProps> = ({ tag, text }) => {
-  return <div role={tag || "h1"}>{text}</div>;
+const Heading: FC<HeadingProps> = ({ tag, text, className }) => {
+  return (
+    <div className={clsx("", className)} role={tag || "h1"}>
+      {text}
+    </div>
+  );
 };
 
 export default Heading;
